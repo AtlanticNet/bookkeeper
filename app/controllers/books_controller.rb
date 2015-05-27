d649b95f486976eb15d876b27d052293da3c320e
ss <git@push.com>
2015-05-27 01:42:04 -0400

    added book show pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :comment)
+    params.require(:book).permit(:title, :comment, :author)
   end
 end
